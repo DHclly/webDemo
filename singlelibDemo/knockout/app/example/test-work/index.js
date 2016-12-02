8c6226a1da2b28d504e116458d75a3f062638973
DHclly <335121817@qq.com>
2016-12-02 17:53:03 +0800

    ko项目调整结构 Signed-off-by: DHclly <335121817@qq.com>

---
+++ singlelibDemo/knockout/app/example/test-work/index.js
@@ -0,0 +1,20 @@
+function AppData() {
+    this.firstName = ko.observable('John');
+    this.lastName = ko.observable('Burns');
+    this.prefix = ko.observable('Dr.');
+    this.computedLog = ko.observable('Log: ');
+    this.fullName = ko.computed(function () {
+        var value = this.prefix() + " " + this.firstName() + " " + this.lastName();
+        // Normally, you should avoid writing to observables within a pure computed 
+        // observable (avoiding side effects). But this example is meant to demonstrate 
+        // its internal workings, and writing a log is a good way to do so.
+        this.computedLog(this.computedLog.peek() + value + '; ');
+        return value;
+    }, this);
+ 
+    this.step = ko.observable(0);
+    this.next = function () {
+        this.step(this.step() === 2 ? 0 : this.step()+1);
+    };
+};
+ko.applyBindings(new AppData());
